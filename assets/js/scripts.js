d9ba0534d66de2ce3308a74f08bfe532143b3fb7
Josue Garcia <72401872+JosueIT@users.noreply.github.com>
2024-04-26 18:11:15 -0600

    whit pdf

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -4,7 +4,7 @@ function abrirPdf(categoria) {
             window.open('', '_blank');
             break;
         case 'cocinas':
-            window.open('https://docs.google.com/viewerng/viewer?url=https://piacere-warehouseclearencesale.netlify.app/assets/pdf/SESCOCINASRED', '_blank');
+            window.open('https://docs.google.com/viewerng/viewer?url=https://piacere-warehouseclearencesale.netlify.app/assets/pdf/SESCOCINASRED.pdf', '_blank');
             break;
         case 'banos':
             window.open('https://docs.google.com/viewerng/viewer?url=/assets/pdf/banos.pdf', '_blank');
