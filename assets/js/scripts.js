3d1cabcfaab8c84526672abb04111301991315cc
Josue Garcia <72401872+JosueIT@users.noreply.github.com>
2024-04-26 18:44:41 -0600

    NUEVO

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,13 +1,13 @@
 function abrirPdf(categoria) {
     switch (categoria) {
         case 'mobiliario':
-            window.open('https://docs.google.com/viewerng/viewer?url=https://piacere-warehouseclearencesale.netlify.app/assets/pdf/SESMOBILIARRED.pdf', '_blank');
+            window.open('https://docs.google.com/viewerng/viewer?url=https://piacere-warehouseclearencesale.netlify.app/assets/pdf/SESMOBILIARRED.pdf&embedded=true', '_blank');
             break;
         case 'cocinas':
-            window.open('https://docs.google.com/viewerng/viewer?url=https://piacere-warehouseclearencesale.netlify.app/assets/pdf/SESCOCINASRED.pdf', '_blank');
+            window.open('https://docs.google.com/viewerng/viewer?url=https://piacere-warehouseclearencesale.netlify.app/assets/pdf/SESCOCINASRED.pdf&embedded=true', '_blank');
             break;
         case 'banos':
-            window.open('https://docs.google.com/viewerng/viewer?url=https://piacere-warehouseclearencesale.netlify.app/assets/pdf/SESBANOSRED.pdf', '_blank');
+            window.open('https://docs.google.com/viewerng/viewer?url=https://piacere-warehouseclearencesale.netlify.app/assets/pdf/SESBANOSRED.pdf&embedded=true', '_blank');
             break;
         default:
             console.log('Categoría no reconocida');
