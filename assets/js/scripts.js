cbe719dbf3175d8a84895bf2b2807be9dc265e0f
Josue Garcia <72401872+JosueIT@users.noreply.github.com>
2024-04-29 16:03:17 -0600

    nuevos links

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,13 +1,13 @@
 function abrirPdf(categoria) {
     switch (categoria) {
         case 'mobiliario':
-            window.open('https://www.dropbox.com/scl/fi/injbmtuon45m6ypa79d25/SESMOBILIARIOREDPDF.pdf?rlkey=5am1uyp1bm2v4caijyrmlje6w&st=cjmlncsj&dl=0', '_blank');
+            window.open('https://www.dropbox.com/scl/fi/cw325r5jr1s7b0m4ei83l/SESMOBILIARIOREDPDF.pdf?rlkey=bl4y6datbccg1cvbb07g2h600&st=kj6eg3dt&dl=0', '_blank');
             break;
         case 'cocinas':
-            window.open('https://www.dropbox.com/scl/fi/tf3tpif7lnlh4qaz3v4lo/SESCOCINASRED.pdf?rlkey=g40iz1he1vpiojpaamdt4cuyg&st=12z7l74k&dl=0', '_blank');
+            window.open('https://www.dropbox.com/scl/fi/6s2vh33i0x98pk3mbwvl1/SESCOCINASRED.pdf?rlkey=scyebibsaen47fe913fqtwny4&st=vpp0z974&dl=0', '_blank');
             break;
         case 'banos':
-            window.open('https://www.dropbox.com/scl/fi/dwyl35hplaamdy15nve70/SESBANOSRED.pdf?rlkey=6jsi8oj25myqi5uhao8qrzlri&st=65u15xoo&dl=0', '_blank');
+            window.open('https://www.dropbox.com/scl/fi/o5yx519nld4ce2bx5lvhi/SESBANOSRED.pdf?rlkey=4jvqqii4x2tkwwhs3zfg2og6x&st=pv97kknm&dl=0', '_blank');
             break;
         default:
             console.log('Categoría no reconocida');
