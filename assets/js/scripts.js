d21e444099a6efcb5d9171615cecb780dcc317f7
Josue Garcia <72401872+JosueIT@users.noreply.github.com>
2024-04-30 12:39:12 -0600

    nuevos links con descarga

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -4,10 +4,10 @@ function abrirPdf(categoria) {
             window.open('https://piacere-warehouseclearencesale.netlify.app/assets/pdf/SESMOBILIARIOREDPDF.pdf', '_blank');
             break;
         case 'cocinas':
-            window.open('https://www.dropbox.com/scl/fi/6s2vh33i0x98pk3mbwvl1/SESCOCINASRED.pdf?rlkey=scyebibsaen47fe913fqtwny4&st=vpp0z974&dl=0', '_blank');
+            window.open('https://piacere-warehouseclearencesale.netlify.app/assets/pdf/SESCOCINASRED.pdf', '_blank');
             break;
         case 'banos':
-            window.open('https://www.dropbox.com/scl/fi/o5yx519nld4ce2bx5lvhi/SESBANOSRED.pdf?rlkey=4jvqqii4x2tkwwhs3zfg2og6x&st=pv97kknm&dl=0', '_blank');
+            window.open('https://piacere-warehouseclearencesale.netlify.app/assets/pdf/SESBANOSRED.pdf', '_blank');
             break;
         default:
             console.log('Categoría no reconocida');
