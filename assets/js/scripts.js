82687d913c9d89cf0ee8efdb76b37fbe150fd8aa
Josue Garcia <72401872+JosueIT@users.noreply.github.com>
2024-04-30 12:29:16 -0600

    new pdfs

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,7 +1,7 @@
 function abrirPdf(categoria) {
     switch (categoria) {
         case 'mobiliario':
-            window.open('https://www.dropbox.com/scl/fi/cw325r5jr1s7b0m4ei83l/SESMOBILIARIOREDPDF.pdf?rlkey=bl4y6datbccg1cvbb07g2h600&st=kj6eg3dt&dl=0', '_blank');
+            window.open('https://piacere-warehouseclearencesale.netlify.app/assets/pdf/SESMOBILIARIOREDPDF.pdf', '_blank');
             break;
         case 'cocinas':
             window.open('https://www.dropbox.com/scl/fi/6s2vh33i0x98pk3mbwvl1/SESCOCINASRED.pdf?rlkey=scyebibsaen47fe913fqtwny4&st=vpp0z974&dl=0', '_blank');
