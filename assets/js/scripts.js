0ef7d82d4a5ac87345b6c6ee06d1390d43347975
Josue Garcia <72401872+JosueIT@users.noreply.github.com>
2024-08-30 11:19:49 -0600

    scripts red pdf

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,7 +2,7 @@
 function abrirPdf(categoria) {
     switch (categoria) {
         case 'mobiliario':
-            window.open('https://piacere-warehouseclearencesale.netlify.app/assets/pdf/SESMOBILIARIOREDPDF.pdf', '_blank');
+            window.open('https://piacere-warehouseclearencesale.netlify.app/assets/pdf/SESMOBILIARIORED.pdf', '_blank');
             break;
         case 'cocinas':
             window.open('https://piacere-warehouseclearencesale.netlify.app/assets/pdf/SESCOCINASRED.pdf', '_blank');
